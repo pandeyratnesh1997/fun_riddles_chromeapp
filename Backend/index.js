94140b685f15e14318292ba403acdb57777b6aaf
Kundan Kumar <kd181197@gmail.com>
2022-09-01 12:43:37 +0530

    index file ready

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const connection = require("./Config/database_config")
 require("dotenv").config();
-
 const app = express();
+app.use(express.json());
+//route Controller
+const riddleRoute = require("./Controller/riddle.controller");
+const userRoute = require("./Controller/user.controller")
+
 app.get("/",(req,res)=>{
     res.send("riddlesapi")
 })
 
+app.use("/user",userRoute)
+app.use("/riddle",riddleRoute);
 
 app.listen(process.env.PORT,async()=>{
     try {
