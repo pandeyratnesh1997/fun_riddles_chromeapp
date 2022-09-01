85c178c96ea464bc19ab68453ab31d0a6f00094c
Ratnesh <pandeyratnesh1997@gmail.com>
2022-09-01 14:11:17 +0530

    admin page redux logic

--- Frontend/funny_riddles/src/Redux/store.js
+++ Frontend/funny_riddles/src/Redux/store.js
@@ -1,8 +1,11 @@
 // store
 import {legacy_createStore, combineReducers, compose,applyMiddleware} from 'redux';
+import thunk from 'redux-thunk'
+import {reducer as adminReducer} from './Admin/reducer';
+
+const rootReducer = combineReducers({adminReducer})
 
 
-
-const store = legacy_createStore();
+const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 
 export default store
