5bf9b55528c2f7599089a8f75a64a8ba42942466
Ratnesh <pandeyratnesh1997@gmail.com>
2022-09-02 21:12:23 +0530

    riddle.jsx issue identified

--- Frontend/funny_riddles/src/Components/GetRiddles.jsx
+++ Frontend/funny_riddles/src/Components/GetRiddles.jsx
@@ -1,54 +1,64 @@
-import { Box, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
-import {EditIcon, DeleteIcon} from '@chakra-ui/icons'
-import React, { useEffect } from 'react';
-import {useNavigate} from 'react-router-dom'
-import { useSelector,useDispatch } from 'react-redux';
+import {
+  Box,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
-import { getData } from '../Redux/Admin/action';
+import { getData } from "../Redux/Admin/action";
 
 const GetRiddles = () => {
-   const riddles = useSelector((state)=>state.adminReducer.data);
-   console.log(riddles)
-const navigate = useNavigate();
-const dispatch =  useDispatch();
+  const riddles = useSelector((state) => state.adminReducer.data);
+  console.log(riddles);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-useEffect(()=>{
-   
-    dispatch(getData())
-},[])
-
+  useEffect(() => {
+    dispatch(getData());
+  }, []);
 
   return (
     <Box m={"auto"} padding="10">
-        <TableContainer>
-            <Table variant={'simple'}>
-                <Thead>
-                    <Tr>
-                        <Th>Riddle</Th>
-                        <Th>Answer</Th>
-                        <Th>creator</Th>
-                        <Th>Edit</Th>
-                        <Th>Delete</Th>
-                      
-                    </Tr>
-                </Thead>
-                <Tbody>
-                        {riddles?.map((el)=>{
-                            return(
-                                <Tr key={el._id}>
-                                    <Td>{el.riddle}</Td>
-                                    <Td>{el.answer}</Td>
-                                    <Td>{el.creator}</Td>
-                                    <Td onClick={()=>navigate(`/riddle/patch/${el._id}`)}><EditIcon/></Td>
-                                    <Td onClick={()=>navigate(`/riddle/delete/${el._id}`)}><DeleteIcon/></Td>
-                                </Tr>
-                            )
-                        })}
-                </Tbody>
-            </Table>
-        </TableContainer>
+      <TableContainer w={"100%"}>
+        <Table w={"100%"} variant={"simple"}>
+          <Thead>
+            <Tr>
+              <Th>Riddle</Th>
+              <Th>Answer</Th>
+              <Th>creator</Th>
+              <Th>Edit</Th>
+              <Th>Delete</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {riddles?.map((el) => {
+              return (
+                <Tr key={el._id}>
+                  <Td>{el.riddle}</Td>
+                  <Td>{el.answer}</Td>
+                  <Td>{el.creator}</Td>
+                  <Td onClick={() => navigate(`/riddle/patch/${el._id}`)}>
+                    <EditIcon />
+                  </Td>
+                  <Td onClick={() => navigate(`/riddle/delete/${el._id}`)}>
+                    <DeleteIcon />
+                  </Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Box>
-  )
-}
+  );
+};
 
-export default GetRiddles
+export default GetRiddles;
--- Frontend/funny_riddles/src/Pages/Riddles.jsx
+++ Frontend/funny_riddles/src/Pages/Riddles.jsx
@@ -1,48 +1,146 @@
-import { Box, HStack } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react';
-import styles from './Styles/Riddles.module.css'
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchRiddle } from '../Redux/Player/action';
+import { Box, Flex, HStack } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
+import React, { useEffect, useState, useRef } from "react";
+import styles from "./Styles/Riddles.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRiddle } from "../Redux/Player/action";
 
 const Riddles = () => {
-    const [page, setpage] = useState(0);
-    const riddle = useSelector((state)=> state.playerReducer);
-    console.log(riddle);
-const dispatch = useDispatch();
+  const [page, setpage] = useState(0);
+  const riddle = useSelector((state) => state.playerReducer);
+  console.log(riddle);
+  const ref1 = useRef(null);
+//   const ref2 = useRef(null);
+//   const ref3 = useRef(null);
+//   const ref4 = useRef(null);
+  const [result, setResult] = useState("");
+  const [ans,setans] = useState("");
+  
 
-useEffect(()=>{
-    dispatch(fetchRiddle(page))
-},[page])
-const CheckResult =()=>{
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchRiddle(page));
+  }, [page]);
+  const CheckResult = () => {
     // checking logic
+const value =ReactDOM.findDOMNode(this)
     // write and wrong
-
+    let userValue;
+   console.log(value);
+    switch (id) {
+      case 1: {
+        userValue = ref1.current.innerText;
+        console.log("1",ref1.current.innerText.trim());
+        console.log("2",riddle.playerRiddle[0].answer.trim())
+        if (userValue.trim() === riddle.playerRiddle[0].answer.trim()) {
+          setResult("Right");
+        } else {
+          setResult("Wrong");
+        }
+        break;
+      }
+      case 2: {
+        userValue = ref1.current.innerText;
+        console.log("1",ref1.current.innerText.trim());
+        console.log("2",riddle.playerRiddle[0].answer.trim())
+        if (userValue.trim() === riddle.playerRiddle[0].answer.trim()) {
+          setResult("Right");
+        } else {
+          setResult("Wrong");
+        }
+        break;
+      }
+      case 3: {
+        userValue = ref1.current.innerText;
+        console.log("1",ref1.current.innerText.trim());
+        console.log("2",riddle.playerRiddle[0].answer.trim())
+        if (userValue.trim() === riddle.playerRiddle[0].answer.trim()) {
+          setResult("Right");
+        } else {
+          setResult("Wrong");
+        }
+        break;
+      }
+      case 4: {
+        userValue = ref1.current.innerText;
+        console.log("1",ref1.current.innerText.trim());
+        console.log("2",riddle.playerRiddle[0].answer.trim())
+        if (userValue.trim() === riddle.playerRiddle[0].answer.trim()) {
+          setResult("Right");
+        } else {
+          setResult("Wrong");
+        }
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+console.log(result);
     // setTimeout()
-    setpage(page+1)
-}
-
+    setTimeout(()=>{
+        setpage(page + 1);
+    },3000)
+    
+  };
+  if (riddle.isLoading) {
+    return <Spinner />
+  } 
+  else if (riddle.isError) {
+    return <Box>Something went wrong!</Box>;
+  }
   return (
-    <Box className={styles.cont}>
-        <Box className={styles.topDiv}>
-            <Box className={styles.score}></Box>
-            <Box className={styles.riddleDiv}></Box>
-        </Box>
-        <Box className={styles.bottomDiv}>
-            <HStack>
-                <Box onClick={CheckResult} className={styles.firstOption}></Box>
-                <Box onClick={CheckResult} className={styles.secondOption}></Box>
-            </HStack>
-            <HStack>
-                <Box onClick={CheckResult} className={styles.thirdOption}></Box>
-                <Box onClick={CheckResult} className={styles.fourthOption}></Box>
-            </HStack>
-        </Box>
-    </Box>
-  )
-}
+    <div>
+      {riddle.playerRiddle.map((el) => {
+        return (
+          <Box key={el._id} className={styles.cont}>
+            <Box className={styles.topDiv}>
+              <Box className={styles.score}></Box>
+              <Box className={styles.riddleDiv}>{el.riddle}</Box>
+            </Box>
+            <Box className={styles.bottomDiv}>
+              <HStack>
+                <Flex
+                  onClick={ this.CheckResult}
+                  ref={ref1}
+                  className={styles.firstOption}
+                >
+                  {el.options[0]}
+                </Flex>
+                <Box
+                  onClick={() => CheckResult(2)}
+                  ref={ref1}
+                  className={styles.secondOption}
+                >
+                  {el.options[1]}
+                </Box>
+              </HStack>
+              <HStack>
+                <Box
+                  onClick={() => CheckResult(3)}
+                  ref={ref1}
+                  className={styles.thirdOption}
+                >
+                  {el.options[2]}
+                </Box>
+                <Box
+                  onClick={() => CheckResult(4)}
+                  ref={ref1}
+                  className={styles.fourthOption}
+                >
+                  {el.options[3]}
+                </Box>
+              </HStack>
+            </Box>
+          </Box>
+        );
+      })}
+  </div>
+  );
+  
+};
 
-export default Riddles
+export default Riddles;
 
-
-// Apple Chancery, cursive
+//
